7eedc04d71d14af107d0410938f462c6f93bd199
kaeriv93 <vsan93@yahoo.com>
2022-05-02 14:01:21 -0500

    first

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const PORT = 4000;
+const app = express();
+const date = require(__dirname + '/date.js');
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended:false}));
+app.use(express.static('public'))
+
+
+
+app.get('/', (req,res)=>{   
+ 
+    
+    res.render('index', {listTitle: date, newListItem: items});
+})
+
+app.post('/', (req,res)=>{
+    const item = req.body.newItem;
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect('/work')
+    }else{
+
+        items.push(item)
+        res.redirect('/');
+    } 
+})
+
+
+app.get('/work', (req,res)=>{
+    res.render('index', {listTitle: "Work List", newListItem: workItems });
+})
+
+
+app.get('/about', (req,res)=>{
+    res.render('about.ejs')
+})
+
+// app.post('/work', (req,res)=>{
+//     let item = req.body.newItem;
+//     workItems.push(item);
+//     res.redirect('/work')
+// })
+
+app.listen(PORT,()=>{
+    console.log(`This port is located on ${PORT} welcome aboard and enjoy`)
+})
---
+++ date.js
@@ -0,0 +1,15 @@
+
+const getDate = ()=>{
+
+
+    const today = new Date();
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+    return today.toLocaleDateString('en-US', options); 
+    
+}
+
+module.exports = getDate();
